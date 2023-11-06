32a5ade2ec491ddf4fd4044740ecbec4464a8b76
Łukasz Sadowski <Lukasz.sadowski2605@gmail.com>
2023-11-06 10:08:10 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <h1>Reaction Timer App</h1>
+  <Box :class="boxColor" :timerDelay="delay" v-if="isPlaying" />
+  <button class="start" :disabled="isPlaying" @click="start">Start</button>
+</template>
+
+<script>
+import Box from "./components/Box.vue";
+export default {
+  name: "App",
+  components: { Box },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      boxColor: "crimson",
+    };
+  },
+  methods: {
+    start() {
+      (this.isPlaying = true), (this.delay = 2000 + Math.random() * 5000);
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+h1 {
+  font-size: 40px;
+  margin-bottom: 50px;
+}
+.start {
+  width: 200px;
+  height: 50px;
+  border-radius: 10px;
+  background-color: rgb(219, 208, 188);
+  cursor: pointer;
+  border: 1px solid grey;
+  font-size: 20px;
+  text-transform: uppercase;
+  transition: all 0.5s;
+}
+.start:hover {
+  background-color: green;
+  color: #fff;
+}
+</style>
---
+++ src/components/Box.vue
@@ -0,0 +1,40 @@
+<template>
+  <div v-if="showBox" class="box">Click</div>
+</template>
+
+<script>
+export default {
+  props: ["timerDelay"],
+  data() {
+    return {
+      boxColor: this.color,
+      showBox: false,
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showBox = true;
+    }, this.timerDelay);
+  },
+  updated() {
+    console.log("zrobione");
+  },
+};
+</script>
+
+<style>
+.box {
+  width: 500px;
+  height: 250px;
+  background-color: crimson;
+  border-radius: 20px;
+  margin-bottom: 50px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 0px;
+  color: #fff;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+</style>
